ffa19e214a9dc5b37a51092dd1047a76ff795276
Micah Courey & Marvin Nikijuluw <student@epicodus.com>
2015-08-03 10:14:00 -0700

    first commit

---
+++ car_dealership.php
@@ -0,0 +1,13 @@
+<?php
+
+    class Car
+    {
+    public $make_model;
+    public $price;
+    public $miles;
+    }
+
+    function __construct($make_model, $price, $miles)
+    {
+    $this->
+}
